6939fc66fd55a5ac59f954ceb9de2b2aacdb2867
Leilanny Nascimento <usilva055@gmail.com>
2022-11-09 20:31:28 -0300

    first commit

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const path = require('path')
 const basePath = path.join(__dirname, 'templates')
 
 const users = require('./users')
+const curse = require('./curse')
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 app.use('/users', users)
+app.use('/curse', curse)
 app.listen(port, () => {
     console.log('Pode comemorar! Server Rodando!')
 })
